c4de8264d78b1bbf5c94cdba855036f48f2d5e05
tienb1910001 <tienb1910001@student.ctu.edu.vn>
2023-03-31 16:47:18 +0700

    Tao trang them moi mot lien he

--- src/router/index.js
+++ src/router/index.js
@@ -16,11 +16,19 @@ const routes = [
         name: "contact.edit",
         component: () => import("@/views/ContactEdit.vue"),
         props: true // Truyền các biến trong $route.params vào làm props
-        },
-        
+    },
+    {
+        path: "/",
+        name: "contact.add",
+        component: () => import("@/views/ContactAdd.vue"),
+        // props: true // Truyền các biến trong $route.params vào làm props
+    },
+
 ];
+
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes,
 });
+
 export default router;
